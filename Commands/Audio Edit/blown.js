428c17d5d8aa08fc42bd78ec0d375cd387692f43
Yakashi13 <83496819+Yakashi13@users.noreply.github.com>
2023-03-04 17:00:51 +0530

    Add files via upload

---
+++ Commands/Audio Edit/blown.js
@@ -0,0 +1,31 @@
+const { exec, spawn, execSync } = require("child_process")
+const fs = require("fs");
+const {getRandom}=require('../../lib/myfunc')
+
+module.exports = {
+    name: "blown",
+    alias: ["blowneffect"],
+    desc: "To add blown effect in a song",
+    category: "Audio Edit",
+    usage: "blown <reply to audio>",
+    react: "🍁",
+    start: async (Miku, m, { text, prefix,quoted,pushName,mime,body }) => {
+
+     let media = await Miku.downloadAndSaveMediaMessage(quoted)
+     let set = '-af acrusher=.1:1:40:0:log'
+     let ran = getRandom('.mp3')
+     try{
+        exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+            fs.unlinkSync(media)
+            if (err) return m.reply('An error Occurd !')
+            let buff = fs.readFileSync(ran)
+            Miku.sendMessage(m.from, { audio: buff, mimetype: 'audio/mpeg' }, { quoted : m })
+            fs.unlinkSync(ran)
+            })
+
+     }catch(e){
+         m.reply('An error Occurd ! Please mention an Audio!')
+        }
+        
+    }
+}
---
+++ Commands/Audio Edit/reverse.js
@@ -0,0 +1,31 @@
+const { exec, spawn, execSync } = require("child_process")
+const fs = require("fs");
+const {getRandom}=require('../../lib/myfunc')
+
+module.exports = {
+    name: "reverse",
+    alias: ["reverseeffect"],
+    desc: "To add reverse effect in a song",
+    category: "Audio Edit",
+    usage: "reverse <reply to audio>",
+    react: "🍁",
+    start: async (Miku, m, { text, prefix,quoted,pushName,mime,body }) => {
+
+     let media = await Miku.downloadAndSaveMediaMessage(quoted)
+     let set = '-filter_complex "areverse"'
+     let ran = getRandom('.mp3')
+     try{
+        exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+            fs.unlinkSync(media)
+            if (err) return m.reply('An error Occurd !')
+            let buff = fs.readFileSync(ran)
+            Miku.sendMessage(m.from, { audio: buff, mimetype: 'audio/mpeg' }, { quoted : m })
+            fs.unlinkSync(ran)
+            })
+
+     }catch(e){
+         m.reply('An error Occurd ! Please mention an Audio!')
+        }
+        
+    }
+}
