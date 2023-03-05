8ec5281c3ddbb3b35363f5102421deeb261b5275
yasas <83496819+Yakashi13@users.noreply.github.com>
2023-03-05 16:23:19 +0530

    Update script.js

--- Commands/Core/script.js
+++ Commands/Core/script.js
@@ -12,7 +12,7 @@ module.exports = {
         let picURL = fs.readFileSync('./Page/yaka.jpg')
         let repoInfo = await axios.get('https://api.github.com/repos/Yakashi13/Yaka-MD')
         let repo = repoInfo.data
-        let txt = `      ⭕️ *${botName}'s Script* ⭕️\n\n*🔄 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Thanks For Using Yaka - MD ❞\n\n*©️ 𝖄𝖆𝖐𝖆𝖘𝖍𝖎 - 2023*`
+        let txt = `      ⭕️ *| Y A K A  B O T's Script |* ⭕️\n\n*🔄 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:*\n${repo.html_url}\n\n❝ Thanks For Using *Y A K A - M D.* ❞\n\n*©️ 𝖄𝖆𝖐𝖆𝖘𝖍𝖎 - 2023*`
         await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
     }
 }
