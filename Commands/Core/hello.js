4233136c6c7ea0b91f68aa376b3c1cbd23e68292
Yasas Vishmitha <83496819+Yakashi13@users.noreply.github.com>
2023-03-06 13:39:43 +0530

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -1,10 +1,21 @@
 module.exports = {
     name: "hi",
-    alias: ["hello", "hey", "yoo", "moshimoshi", "alive", "active"],
+    alias: ["hello","alive","ping","moshimoshi","yoo","konichiwa"],
     desc: "Say hello to bot.",
     react: "💜",
     category: "Core",
-    start: async (Miku, m, { pushName, prefix }) => {
-        await Miku.sendMessage(m.from, { text: `\n*_Konnichiwa ${pushName} Senpai_ 😛*  !!,\n\n*I am  ${botName}  bot 🤗.*\n\n *Soo, How can I help you ❔❔*\n\nType anything with  ${prefix}  to respond.` }, { quoted: m })
+    start: async(Miku, m,{pushName,prefix}) => {
+        const pad = (s) => (s < 10 ? "0" : "") + s;
+        const formatTime = (seconds) => {
+        const hours = Math.floor(seconds / (60 * 60));
+        const minutes = Math.floor((seconds % (60 * 60)) / 60);
+        const secs = Math.floor(seconds % 60);
+        return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+        };
+        const uptime = () => formatTime(process.uptime());
+        await Miku.sendMessage(m.from,{image:{url:botImage3},caption:`\n*Konnichiwa ${pushName} -Kun ❤️‍🔥 !!, I am ${botName} bot*.
+        \nType *${prefix}help* to get my full command list.
+        \n\n_📶 Server Uptime:_ *${uptime()}*
+        \n_💯 Status:_ *Stable*\n`},{quoted:m})
     }
 }
