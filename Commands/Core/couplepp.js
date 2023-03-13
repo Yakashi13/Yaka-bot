002f7238a2d739510c036bd6209302f16aa4df5b
Yasas Vishmitha <83496819+Yakashi13@users.noreply.github.com>
2023-03-13 13:07:54 +0530

    Update couplepp.js

--- Commands/Core/couplepp.js
+++ Commands/Core/couplepp.js
@@ -1,16 +1,15 @@
-const {fetchJson} = require('../../lib/myfunc')
+const axios = require("axios")
 
 module.exports = {
     name: "couplepp",
-    alias: ["ppcouple","couplepfp"],
+    alias: ["ppcouple"],
     desc: "Get matching couple profile picture.",
     react: "💞",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
-        let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json');
-        let randompplink = anu[Math.floor(Math.random() * anu.length)];
-        Miku.sendMessage(m.from, { image: { url: randompplink.male }, caption: `_For Him..._` }, { quoted: m })
-        Miku.sendMessage(m.from, { image: { url: randompplink.female }, caption: `_For Her..._` }, { quoted: m })
+        let shibam = await axios.get('https://neko-couple-api.onrender.com');
+        Miku.sendMessage(m.from, { image: { url: shibam.data.male }, caption: `_For Him..._` }, { quoted: m })
+        Miku.sendMessage(m.from, { image: { url: shibam.data.female }, caption: `_For Her..._` }, { quoted: m })
 
     }
 }
