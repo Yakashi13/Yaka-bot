fde8684b722accffe97784875489245aa3de9d5d
Yasas Vishmitha <83496819+Yakashi13@users.noreply.github.com>
2023-03-30 21:54:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ if (!gg) {
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || ""; // paste your own MongoDB url..
+global.mongodb = process.env.MONGODB || "mongodb+srv://yakamd:yakkubolaw@cluster0.x7fe21k.mongodb.net/?retryWrites=true&w=majority"; // paste your own MongoDB url..
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || ".";
 global.tenorApiKey =
