ef69964f491bfe7942104c2f11575ec78aaeb736
Yasas Vishmitha <83496819+Yakashi13@users.noreply.github.com>
2023-04-09 01:25:25 +0530

    Update trivia.js

--- Commands/Essentials/trivia.js
+++ Commands/Essentials/trivia.js
@@ -5,7 +5,7 @@ module.exports = {
   alias: ["question"],
   desc: "To get some question answers.",
   usage: "question easy/medium/hard",
-  react: "👹",
+  react: "🍁",
   category: "Essentials",
   start: async (Miku, m, { pushName, prefix, args, text }) => {
     if (!args[0]) return m.reply(`Please use this command like this: ${prefix}question easy/medium/hard`);
@@ -13,13 +13,22 @@ module.exports = {
     await axios
       .get(`https://opentdb.com/api.php?amount=1&difficulty=${query}&type=multiple`).then((response) => {
 
-        const text = `*『  Question Answers  』*\n\n🍃  *Category:* ${response.data.results[0].category}\n❄  *Difficulty:* ${response.data.results[0].difficulty}\n\n📒  *Question:* ${response.data.results[0].question}\n\n🎋  *Answer:* ${response.data.results[0].correct_answer}\n `;
+        const text = `*『  Question Answers  』*\n\n🎀  *Category:* ${response.data.results[0].category}\n❄  *Difficulty:* ${response.data.results[0].difficulty}\n\n📒  *Question:* ${response.data.results[0].question}\n\n🎋  *Answer:* ${response.data.results[0].correct_answer}\n `;
 
-        Miku.sendMessage(
-          m.from,
-          { image: { url: botImage3 }, caption: text },
-          { quoted: m }
-        );
+        let buttons = [];
+
+        let buttonMessage = {
+          image: { url: botImage3 },
+          caption: text,
+          footer: `*${botName}*`,
+          buttons: buttons,
+          headerType: 4,
+        };
+
+
+
+        Miku.sendMessage(m.from, buttonMessage, { quoted: m });
+      })
       .catch((err) => {
         m.reply(`Please use this command like this: ${prefix}question easy/medium/hard`);
       });
