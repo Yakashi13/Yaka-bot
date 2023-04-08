9ca6ef30b51fd652b778bc668764252434e74a0f
Yasas Vishmitha <83496819+Yakashi13@users.noreply.github.com>
2023-04-08 22:15:05 +0530

    Update trivia.js

--- Commands/Essentials/trivia.js
+++ Commands/Essentials/trivia.js
@@ -13,28 +13,13 @@ module.exports = {
     await axios
       .get(`https://opentdb.com/api.php?amount=1&difficulty=${query}&type=multiple`).then((response) => {
 
-        const text = `            *『  Question Answers  』*\n\n🍃  *Category:* ${response.data.results[0].category}\n❄  *Difficulty:* ${response.data.results[0].difficulty}\n\n📒  *Question:* ${response.data.results[0].question}\n\n🎋  *Answer:* ${response.data.results[0].correct_answer}\n `;
+        const text = `*『  Question Answers  』*\n\n🍃  *Category:* ${response.data.results[0].category}\n❄  *Difficulty:* ${response.data.results[0].difficulty}\n\n📒  *Question:* ${response.data.results[0].question}\n\n🎋  *Answer:* ${response.data.results[0].correct_answer}\n `;
 
-        let buttons = [
-          {
-            buttonId: `${prefix}trivia ${query}`,
-            buttonText: { displayText: `Next ${query} question` },
-            type: 1,
-          },
-        ];
-
-        let buttonMessage = {
-          image: { url: botImage3 },
-          caption: text,
-          footer: `*${botName}*`,
-          buttons: buttons,
-          headerType: 4,
-        };
-
-
-
-        Miku.sendMessage(m.from, buttonMessage, { quoted: m });
-      })
+        Miku.sendMessage(
+          m.from,
+          { image: { url: botImage3 }, caption: text },
+          { quoted: m }
+        );
       .catch((err) => {
         m.reply(`Please use this command like this: ${prefix}question easy/medium/hard`);
       });
