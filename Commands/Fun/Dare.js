55a38020cdb713f851859933f145f0259dd5d269
Yasas Vishmitha <83496819+Yakashi13@users.noreply.github.com>
2023-04-09 21:17:18 +0530

    Update Dare.js

--- Commands/Fun/Dare.js
+++ Commands/Fun/Dare.js
@@ -1,15 +1,17 @@
-const axios = require('axios')
+const axios = require("axios");
 
 module.exports = {
-    name: "dare",
-    alias: ["givedare","d"],
-    desc: "give a dare",
-    cool:3,
-    react: "🙄",
-    category: "Fun",
-    start: async(Miku, m,{text, prefix}) => {
-
-     
-      
-    const shibam = await axios.get('https://dull-plum-panda-gear.cyclic.app/dare')
+  name: "dare",
+  alias: ["givedare", "d"],
+  desc: "give a dare",
+  cool: 3,
+  react: "🙄",
+  category: "Fun",
+  start: async (Miku, m, { text, prefix }) => {
+    const shibam = await axios.get(
+      "https://dull-plum-panda-gear.cyclic.app/dare"
+    );
+    
     await Miku.sendMessage(m.from, {image: { url: botImage4 },caption: `*${shibam.data}*`,}, { quoted: m });
+  },
+};
